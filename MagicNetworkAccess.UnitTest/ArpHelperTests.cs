9fd210abbc52efe254045a213b92329c2cbe3719
Stefan Lampert <das_gigabike@hotmail.com>
2016-05-20 22:32:10 +0200

    add simple test

---
+++ MagicNetworkAccess.UnitTest/ArpHelperTests.cs
@@ -0,0 +1,16 @@
+﻿using MagicNetworkAccess.Library.Helper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MagicNetworkAccess.UnitTest
+{
+    [TestClass]
+    public class ArpHelperTests
+    {
+        [TestMethod]
+        public void GetArpTable()
+        {
+            Assert.IsNotNull(ArpHelper.GetArpTable());
+        }
+    }
+}
